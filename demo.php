3b21230b9196d266e0e9963388f4f95333c0c060
DreamLess <swajan.cse.ku@gmail.com>
2018-02-11 02:56:20 +0600

    calculator added double click on comission input

--- demo.php
+++ demo.php
@@ -1,82 +1,62 @@
 <?php
-include('./templates/head.php');
-require_once('./bll/bll.demo.php');
-
-
+    include_once './templates/topper-customized.php';
 ?>
-<div class="wrapper">
-    <!--Sidebar here-->
-<?php
-    // Dashboard Sidebar
-include('./templates/sidebar.php');
+<script>
 
-?>
-<div class="main-panel">
-
-<?php
-            // Top Navbar
-include('./templates/navbar.php');
-?>
-
-<!--Page contend starts here .. seperated in each file -->
-<div class="content">
-<div class="container-fluid">
-    <div class="row">
-
-<?php
-if (isset($_SESSION['message']))
-    {
-        $info= '<div class="alert alert-info">';
-        $info.='<span>'.$_SESSION['message'].'</span>';
-        $info.='</div>';
-        echo $info;
-        unset($_SESSION['message']);
+    function showCalculator() {
+        $('#calc').show();
     }
-?>
 
-    <!-- Data Display -->
-    <div class="col-md-12">
-        <div class="card">
-            <div class="header">
-                <h3 class="title text-center"><b>Demo data</b></h3>
-            </div>
+</script>
+<div style="position: relative">
+    <input type="button" value="Calculator" onclick="showCalculator()">
+    <input type="number" id="comission">
 
-            <div class="content table-responsive table-full-width">
-            <table class="table table-hover table-striped" id="SalesReport">
-	            <thead>
-	                <th>#</th>
-					<th>Name</th>
-					<th>Password</th>
-	            </thead>
-	            <tbody>
-	            	<tbody>
-					 <?php
-					 	$bllDemo = new BLLDemo;
-					 	echo $bllDemo->getUsers();
+    <FORM NAME="Calc" id="calculator" style="display: block;z-index: 1;position: absolute;top: 80px;left: 50px;">
+        <TABLE BORDER=4>
+            <TR>
+                <TD>
+                    <INPUT TYPE="text"   NAME="Input" Size="16">
+                    <br>
+                </TD>
+            </TR>
+            <TR>
+                <TD>
+                    <INPUT TYPE="button" NAME="ac"   VALUE="AC" OnClick="Calc.Input.value = ''">
+                    <INPUT TYPE="button" NAME="clear"   VALUE="c" OnCLick="Calc.Input.value = Calc.Input.value.slice(0,-1)">
+                    <INPUT TYPE="button" NAME="close" VALUE="close" OnClick="$('#calculator').hide()">
+                    <INPUT TYPE="button" NAME="save"  VALUE="save" OnClick="$('#comission').val(Calc.Input.value);$('#calculator').hide()">
+                    <br>
+                    <INPUT TYPE="button" NAME="one"   VALUE="  1  " OnClick="Calc.Input.value += '1'">
+                    <INPUT TYPE="button" NAME="two"   VALUE="  2  " OnCLick="Calc.Input.value += '2'">
+                    <INPUT TYPE="button" NAME="three" VALUE="  3  " OnClick="Calc.Input.value += '3'">
+                    <INPUT TYPE="button" NAME="plus"  VALUE="  +  " OnClick="Calc.Input.value += ' + '">
+                    <br>
+                    <INPUT TYPE="button" NAME="four"  VALUE="  4  " OnClick="Calc.Input.value += '4'">
+                    <INPUT TYPE="button" NAME="five"  VALUE="  5  " OnCLick="Calc.Input.value += '5'">
+                    <INPUT TYPE="button" NAME="six"   VALUE="  6  " OnClick="Calc.Input.value += '6'">
+                    <INPUT TYPE="button" NAME="minus" VALUE="  --  " OnClick="Calc.Input.value += ' - '">
+                    <br>
+                    <INPUT TYPE="button" NAME="seven" VALUE="  7  " OnClick="Calc.Input.value += '7'">
+                    <INPUT TYPE="button" NAME="eight" VALUE="  8  " OnCLick="Calc.Input.value += '8'">
+                    <INPUT TYPE="button" NAME="nine"  VALUE="  9  " OnClick="Calc.Input.value += '9'">
+                    <INPUT TYPE="button" NAME="times" VALUE="  x  " OnClick="Calc.Input.value += ' * '">
+                    <br>
+                    <INPUT TYPE="button" NAME="point" VALUE="  .   " OnClick="Calc.Input.value += '.'">
+                    <INPUT TYPE="button" NAME="zero"  VALUE="  0  " OnClick="Calc.Input.value += '0'">
+                    <INPUT TYPE="button" NAME="DoIt"  VALUE="  =  " OnClick="Calc.Input.value = eval(Calc.Input.value)">
+                    <INPUT TYPE="button" NAME="div"   VALUE="   /  " OnClick="Calc.Input.value += ' / '">
+                    <br>
+                </TD>
+            </TR>
+        </TABLE>
+    </FORM>
 
-					 	var_dump($bllDemo->getAllUsers());
-
-
-					 ?>
-					</tbody>
-	            </tbody>
-            </table>
-            </div>
-        </div>
-    </div>
-    <!-- Data Display -->
-
-    </div>
 </div>
-</div>
-<link rel="stylesheet" type="text/css" href="assets/css/tableexport.min.css">
 
-<!-- Report csv,xlsx,txt plugins -->
-<script type="text/javascript" src="assets/js/bootstrap.min_1.js"></script>
-<script type="text/javascript" src="assets/js/FileSaver.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="assets/js/tableexport.min.js"></script>
+<h2>Hello</h2>
+
 
 <?php
-    include('./templates/footer.php');
+    include_once './templates/footer-customized.php';
 ?>
--- sales.php
+++ sales.php
@@ -181,7 +181,7 @@ include_once './templates/topper-customized.php';
                         "<div class=\"row\" id='comissionDiv'>\n"+
                         "<div class=\"col-md-8 \"></div>\n"+
                         "<div class=\"col-md-2 \"><h4>Comission : </h4></div>\n" +
-                        "<div class=\"col-md-2 \"><input id='comission' name='comission' value='0' type='number' min='0' class='form-control' oninput='calculateTotalCost()' required></div>\n" +
+                        "<div class=\"col-md-2 \"><input id='comission' name='comission' value='0' type='number' min='0' class='form-control' oninput='calculateTotalCost()'  ondblclick=\"$('#calculator').show()\" required></div>\n" +
                         "</div>";
 //        total cost row
             html    +=  "<div class=\"row\" >\n"+
@@ -335,6 +335,45 @@ include_once './templates/topper-customized.php';
         </div>
         <input type="submit" id="confirmSale" name="confirmSale" class="btn btn-primary" style="margin-left: 40%;display: none" value="Confirm The Sale" >
     </form>
+    <FORM NAME="Calc" id="calculator" style="display: none;z-index: 1;position: fixed;bottom: 10px;right: 50px;">
+        <TABLE BORDER=4>
+            <TR>
+                <TD>
+                    <INPUT TYPE="text"   NAME="Input" Size="20"  readonly>
+                    <br>
+                </TD>
+            </TR>
+            <TR>
+                <TD>
+                    <INPUT TYPE="button" NAME="ac"   VALUE="AC" OnClick="Calc.Input.value = ''">
+                    <INPUT TYPE="button" NAME="clear"   VALUE="c" OnCLick="Calc.Input.value = Calc.Input.value.slice(0,-1)">
+                    <INPUT TYPE="button" NAME="close" VALUE="close" OnClick="$('#calculator').hide();Calc.Input.value=''">
+                    <INPUT TYPE="button" NAME="save"  VALUE="save" OnClick="$('#comission').val(Calc.Input.value);Calc.Input.value='';$('#calculator').hide();calculateTotalCost()">
+                    <br>
+                    <INPUT TYPE="button" NAME="one"   VALUE="  1  " OnClick="Calc.Input.value += '1'">
+                    <INPUT TYPE="button" NAME="two"   VALUE="  2  " OnCLick="Calc.Input.value += '2'">
+                    <INPUT TYPE="button" NAME="three" VALUE="  3  " OnClick="Calc.Input.value += '3'">
+                    <INPUT TYPE="button" NAME="plus"  VALUE="  +  " OnClick="Calc.Input.value += ' + '">
+                    <br>
+                    <INPUT TYPE="button" NAME="four"  VALUE="  4  " OnClick="Calc.Input.value += '4'">
+                    <INPUT TYPE="button" NAME="five"  VALUE="  5  " OnCLick="Calc.Input.value += '5'">
+                    <INPUT TYPE="button" NAME="six"   VALUE="  6  " OnClick="Calc.Input.value += '6'">
+                    <INPUT TYPE="button" NAME="minus" VALUE="  --  " OnClick="Calc.Input.value += ' - '">
+                    <br>
+                    <INPUT TYPE="button" NAME="seven" VALUE="  7  " OnClick="Calc.Input.value += '7'">
+                    <INPUT TYPE="button" NAME="eight" VALUE="  8  " OnCLick="Calc.Input.value += '8'">
+                    <INPUT TYPE="button" NAME="nine"  VALUE="  9  " OnClick="Calc.Input.value += '9'">
+                    <INPUT TYPE="button" NAME="times" VALUE="  x  " OnClick="Calc.Input.value += ' * '">
+                    <br>
+                    <INPUT TYPE="button" NAME="point" VALUE="  .   " OnClick="Calc.Input.value += '.'">
+                    <INPUT TYPE="button" NAME="zero"  VALUE="  0  " OnClick="Calc.Input.value += '0'">
+                    <INPUT TYPE="button" NAME="DoIt"  VALUE="  =  " OnClick="Calc.Input.value = eval(Calc.Input.value)">
+                    <INPUT TYPE="button" NAME="div"   VALUE="   /  " OnClick="Calc.Input.value += ' / '">
+                    <br>
+                </TD>
+            </TR>
+        </TABLE>
+    </FORM>
 </div>
 
 <?php
