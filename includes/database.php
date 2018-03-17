244555dfab9fd873a1dc8117f0f71eed9dde3206
DreamLess <swajan.cse.ku@gmail.com>
2018-03-17 16:50:00 +0600

    halkhata created

--- includes/database.php
+++ includes/database.php
@@ -304,12 +304,12 @@ function db_get_returns($dateFrom,$dateTo){
 }
 
 function db_get_party_due(){
-    $query = "select customer.name ,customer.address,partyduepayment.amount from customer,partyduepayment,party where customer.id=party.customerId && customer.id=partyduepayment.customerId";
+    $query = "select customer.id,customer.name ,customer.address,partyduepayment.amount from customer,partyduepayment,party where customer.id=party.customerId && customer.id=partyduepayment.customerId";
     $rows = db_select($query);
     $html = '<thead>
                 <tr>
                     <th>SL.</th>
-                    <th>Customer Name</th>
+                    <th>Party Name</th>
                     <th>Address</th>
                     <th>Due Amount</th>
                     
@@ -319,7 +319,7 @@ function db_get_party_due(){
     foreach ($rows as $row){
         $html .= '<tr>
                     <td>'.(++$i).'</td>
-                    <td>'.$row['name'].'</td>
+                    <td><a target="_blank" href="/party-due-payment.php?customerId='.$row['id'].'">'.$row['name'].'</a></td>
                     <td>'.$row['address'].'</td>
                     <td>'.$row['amount'].'</td>
                 </tr>';
@@ -328,7 +328,7 @@ function db_get_party_due(){
     return $html;
 }
 
-function db_get_party_product_bought_payment($dateFrom,$dateTo,$customerId){
+function db_get_party_product_bought_payment($dateFrom,$dateTo,$customerId,$halkhataDue){
 //    getting current due
     $query = "select partyduepayment.amount from partyduepayment where partyduepayment.customerId='$customerId'";
     $currentDue = db_select($query)[0]['amount'];
@@ -443,8 +443,22 @@ function db_get_party_product_bought_payment($dateFrom,$dateTo,$customerId){
 
                         </div>
                         <div class="content">
-                            <h4>Due within this date : '.($sale-$payment).' Tk</h4>
-                            <h4>Present Due : '.$currentDue.' Tk</h4>
+                            <h5>Due within this date : '.($sale-$payment).' Tk</h5>
+                           
+                            <table>
+                                <tr>
+                                    <td><h6>Due After Halkhata :</h6></td><td style="text-align: right">'.$halkhataDue.'</td>
+                                </tr>
+                                <tr>
+                                    <td><h6>Product Bought :</h6></td><td style="text-align: right">+&nbsp;'.$sale.'</td>
+                                </tr>
+                                <tr>
+                                    <td><h6>Payment :</h6></td><td style="text-align: right">-&nbsp;'.$payment.'</td>
+                                </tr>
+                                <tr>
+                                    <td><h6>Present Due :</h6></td><td style="text-align: right">'.$currentDue.'</td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>';
